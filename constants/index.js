548af1a3122d78c408369b87579e33e7a56341ed
MoDeFi <130085341+Mo-DeFi@users.noreply.github.com>
2023-07-19 11:48:22 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x6013ed0386dd00589771a961881da19ed2f9a6bc";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
